29861ee557cacb2f600d351bfe389c3c42b6cc95
TruongHieu2003 <hieuvipidol2@gmail.com>
2026-02-26 18:34:35 +0700

    Delete pha-le-love directory

--- pha-le-love/script.js
+++
@@ -1,7 +0,0 @@
-function showLove(){
-  document.getElementById("secretMessage").style.display = "block";
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: "smooth"
-  });
-}
