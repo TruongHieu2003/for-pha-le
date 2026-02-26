04c3a6a18b4d08f102ee5c7aefc8332fe77a8bad
TruongHieu2003 <hieuvipidol2@gmail.com>
2026-02-26 20:06:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,43 @@ function openChapter(number) {
 
     if (number === 1) {
         text.innerHTML = `
-        <!-- DÁN NỘI DUNG CHAPTER 1 Ở ĐÂY -->
-        <p>Viết nội dung chương 1 ở đây...</p>
+
+        <h2 class="chapter-title">Chương 1 — Vì Anh Thích Em</h2>
+
+<p>
+Thật ra… cũng không vì lý do gì hết.  
+Anh thấy thích em thôi.  
+Anh chỉ đơn giản là muốn em làm người yêu anh.
+</p>
+
+<p>
+Những ngày tháng đầu… công nhận là vô tư thật ha.  
+Anh không suy nghĩ gì nhiều.  
+Anh chỉ muốn làm em cười thôi.
+</p>
+
+<p>
+Anh chỉ muốn mỗi ngày đi học đều được thấy em.  
+Được đi căn tin chung.  
+Ăn chung. Uống chung.
+</p>
+
+<p>
+Anh thích nhất là lúc em xem anh đá banh.  
+Có lần anh nghe trên lầu có người kêu tên anh.  
+Anh không quay lên — vì anh biết đó không phải giọng em.
+</p>
+
+<p>
+Nhưng anh biết…  
+Em đang nhìn anh.
+</p>
+
+<p class="highlight">
+Tóm lại là…  
+ANH YÊU EM.
+</p>
+        
         `;
     }
 
@@ -40,3 +75,4 @@ function openChapter(number) {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+
